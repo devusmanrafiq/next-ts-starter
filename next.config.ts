fafdf2221ad1b5194cd6472e657bcfd6aa0183a5
Usman Rafiq <149041976+devusmanrafiq@users.noreply.github.com>
2025-10-03 16:28:42 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,21 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   distDir: 'dist',
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              icon: true,
+            },
+          },
+        ],
+        as: '*.js',
+      },
+    },
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
